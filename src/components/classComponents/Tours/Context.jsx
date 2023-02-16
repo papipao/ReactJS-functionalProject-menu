cf2274914ef5ee76d371685f6fefef1b582bc235
unknown <paolomiguel108@gmail.com>
2023-02-16 21:19:25 +0800

    ReactJS-classProject-menu

--- src/components/classComponents/Tours/Context.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const TourContext = React.createContext();
-
-const TourProvider = TourContext.Provider;
-const TourConsumer = TourContext.Consumer;
-
-export { TourProvider, TourConsumer };
